ba375be8e557aeb419119ed2c1c43a6ca13e9633
Alex Brown <alexbrown@AlexsMacStudio.home>
2022-07-02 21:56:29 -0600

    A lot of stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import SharedLayout from './screens/SharedLayout';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 const App = () => {
   return (
-    <>
-    <h1>Welcome To ProShop</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SharedLayout/>}>
+          <Route index element={<HomeScreen />}/>
+          <Route path='product/:id' element={<ProductScreen />} /> 
+          <Route path='cart' element={<CartScreen />} />
+          <Route path='cart/:id' element={<CartScreen />} />
+          <Route path='cart/:id/:qty' element={<CartScreen />} />
+          <Route path='login' element={<LoginScreen />} />
+          <Route path='register' element={<RegisterScreen />} />
+          <Route path='profile' element={<ProfileScreen />} />
+          <Route path='shipping' element={<ShippingScreen />}/>
+          <Route path='payment' element={<PaymentScreen />}/>
+          <Route path='placeorder' element={<PlaceOrderScreen />}/>
+          <Route path='order/:id' element={<OrderScreen />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
